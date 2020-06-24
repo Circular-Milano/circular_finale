3f5e918f043957dea3f5ab3dd595fdd4e0f29e34
Massimo Galeotti <massi.galex@gmail.com>
2020-06-24 16:07:56 +0200

    routes fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,99 +2,99 @@ class ItemsController < ApplicationController
   #skip_before_action :authenticate_user!, only: [:index, :show]
 
    def index
-    real_parameters = params[:search]
-    @items = Item.all
-    @markers = []
-    return if real_parameters.nil?
+#     real_parameters = params[:search]
+#     @items = Item.all
+#     @markers = []
+#     return if real_parameters.nil?
 
-    @location = real_parameters[:location].downcase.capitalize unless real_parameters[:location].nil? || real_parameters[:location] == ""
+#     @location = real_parameters[:location].downcase.capitalize unless real_parameters[:location].nil? || real_parameters[:location] == ""
 
-    if !real_parameters[:start_time].nil? && real_parameters[:start_time] != ""
-      @full_time = real_parameters[:start_time]
-      @start_time = Date.parse(@full_time.chars.first(10).join)
-      @end_time = Date.parse(@full_time.chars.last(10).join)
-    elsif !@range_time.nil?
-      @start_time = Date.parse(@range_time.chars.first(10).join)
-      @end_time = Date.parse(@range_time.chars.last(10).join)
-    end
+#     if !real_parameters[:start_time].nil? && real_parameters[:start_time] != ""
+#       @full_time = real_parameters[:start_time]
+#       @start_time = Date.parse(@full_time.chars.first(10).join)
+#       @end_time = Date.parse(@full_time.chars.last(10).join)
+#     elsif !@range_time.nil?
+#       @start_time = Date.parse(@range_time.chars.first(10).join)
+#       @end_time = Date.parse(@range_time.chars.last(10).join)
+#     end
 
-    @size = real_parameters[:size] unless real_parameters[:size].nil? || real_parameters[:size] == ""
+#     @size = real_parameters[:size] unless real_parameters[:size].nil? || real_parameters[:size] == ""
 
-    if real_parameters[:start_time].present? && real_parameters[:location].present? && real_parameters[:size].present?
-      @items = Item.joins(:user).where('users.address like ?', "%#{@location}%").where(['start_time < ? AND end_time > ?', @start_time, @end_time]).where(['size = ?', @size])
+#     if real_parameters[:start_time].present? && real_parameters[:location].present? && real_parameters[:size].present?
+#       @items = Item.joins(:user).where('users.address like ?', "%#{@location}%").where(['start_time < ? AND end_time > ?', @start_time, @end_time]).where(['size = ?', @size])
 
-    elsif real_parameters[:start_time].present? && real_parameters[:location].present?
-      @items = Item.joins(:user).where('users.address like ?', "%#{@location}%").where(['start_time < ? AND end_time > ?', @start_time, @end_time])
+#     elsif real_parameters[:start_time].present? && real_parameters[:location].present?
+#       @items = Item.joins(:user).where('users.address like ?', "%#{@location}%").where(['start_time < ? AND end_time > ?', @start_time, @end_time])
 
-    elsif real_parameters[:location].present? && real_parameters[:size].present?
-      @items = Item.joins(:user).where('users.address like ?', "%#{@location}%").where(['size = ?', @size])
+#     elsif real_parameters[:location].present? && real_parameters[:size].present?
+#       @items = Item.joins(:user).where('users.address like ?', "%#{@location}%").where(['size = ?', @size])
 
-    elsif real_parameters[:start_time].present? && real_parameters[:size].present?
-      @items = Item.where(['start_time < ? AND end_time > ?', @start_time, @end_time]).where(['size = ?', @size])
+#     elsif real_parameters[:start_time].present? && real_parameters[:size].present?
+#       @items = Item.where(['start_time < ? AND end_time > ?', @start_time, @end_time]).where(['size = ?', @size])
 
-    elsif real_parameters[:size].present?
-      @items = Item.where(['size = ?', @size])
+#     elsif real_parameters[:size].present?
+#       @items = Item.where(['size = ?', @size])
 
-    elsif real_parameters[:start_time].present?
-      @items = Item.where(['start_time < ? AND end_time > ?', @start_time, @end_time])
+#     elsif real_parameters[:start_time].present?
+#       @items = Item.where(['start_time < ? AND end_time > ?', @start_time, @end_time])
 
-    elsif real_parameters[:location].present?
-      @items = Item.joins(:user).where('users.address like ?', "%#{@location}%")
+#     elsif real_parameters[:location].present?
+#       @items = Item.joins(:user).where('users.address like ?', "%#{@location}%")
 
-    else
-      @items = Item.all
-    end
+#     else
+#       @items = Item.all
+#     end
 
-    users = @items.map { |item| item.user }.uniq
-    unless users.nil?
-      @users = User.where(id: users.map(&:id)).geocoded
-      @markers = @users.map do |user|
-        {
-          lat: user.latitude,
-          lng: user.longitude
-          # infoWindow: render_to_string(partial: "info_window", locals: { user: user })
-        }
-      end
-    end
+#     users = @items.map { |item| item.user }.uniq
+#     unless users.nil?
+#       @users = User.where(id: users.map(&:id)).geocoded
+#       @markers = @users.map do |user|
+#         {
+#           lat: user.latitude,
+#           lng: user.longitude
+#           # infoWindow: render_to_string(partial: "info_window", locals: { user: user })
+#         }
+#       end
+#     end
+   end
+
+   def new
+#     @item = Item.new
+   end
+
+   def create
+#     item = Item.new(item_params)
+#     item.user = current_user
+#     item.save
+#     redirect_to items_show_path
   end
 
-  def new
-    @item = Item.new
-  end
+   def update
+#     @item = Item.find(params[:id])
+#     @item.update(item_params)
+#     redirect_to pages_items_path
+   end
 
-  def create
-    item = Item.new(item_params)
-    item.user = current_user
-    item.save
-    redirect_to items_show_path
-  end
+   def edit
+#     @item = Item.find(params[:id])
+   end
 
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to pages_items_path
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def destroy
-    @item = Item.find(params[:id])
-    @item.update(deleted: true)
-    redirect_to pages_items_path
-  end
+   def destroy
+#     @item = Item.find(params[:id])
+#     @item.update(deleted: true)
+#     redirect_to pages_items_path
+   end
 
   def show
-    @item = Item.find(params[:id])
-    @reviews = Review.all
-    @order = Order.new
+#     @item = Item.find(params[:id])
+#     @reviews = Review.all
+#     @order = Order.new
   end
 
-  #private
+#   #private
 
-  #def item_params
-  #  params.require(:item).permit(:name, :description, :condition, :start_time, :end_time, :price, :photo)
-  #end
+#   #def item_params
+#   #  params.require(:item).permit(:name, :description, :condition, :start_time, :end_time, :price, :photo)
+#   #end
 end
 
