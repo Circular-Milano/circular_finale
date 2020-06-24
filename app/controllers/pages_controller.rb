aee29c93129f41b2bcfa6c8d502a340c9e39e61c
Massimo Galeotti <massi.galex@gmail.com>
2020-06-24 14:14:06 +0200

    pages are working very well. We have to fix items, orders and reviews routes and check controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,23 @@
 class PagesController < ApplicationController
-  def homepage
+  def home
   end
 
   def about_us
   end
+
+  def index
+  end
+
+  def get_the_app
+  end
+
+  def how_it_works
+  end
+
+  def experience
+  end
+
+  def help
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#homepage'
+  root to: "pages#homepage", as: "home"
+  get "/about", to: "pages#about_us", as: "about"
+  get "/index", to: "pages#index", as: "index"
+  get "/get_the_app", to: "pages#get_the_app", as: "get_the_app"
+  get "/how_it_works", to: "pages#how_it_works", as: "how_it_works"
+  get "/experience", to: "pages#experience", as: "experience"
+  get "/help", to: "pages#help", as: "help"
+
+
+  get "/create_item", to: "items#create", as: "create_item"
+  get "/new_item", to: "items#new", as: "new_item"
+  get "/update_item", to: "items#update", as: "update_item"
+  get "/edit_item", to: "items#edit", as: "edit_item"
+  get "/destroy_item", to: "items#destroy", as: "destroy_item"
+  get "/show_item", to: "items#show", as: "show_item"
+
+
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
