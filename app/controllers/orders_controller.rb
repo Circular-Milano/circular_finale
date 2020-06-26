45004633600608619ed4709046f15b759c1c8b57
Massimo Galeotti <massi.galex@gmail.com>
2020-06-26 10:36:13 +0200

    uncomment some stuff in controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,32 +1,32 @@
 class OrdersController < ApplicationController
     def new
-#     @item = Item.find(params[:item_id])
-#     @order = Order.new
+#     @order = Order.find(params[:item_id])
+     @order = Order.new
     end
 
    def create
 #     @order = Order.new(order_params)
-#     @order.user = current_user
+     @order.user = current_user
 #     @order.item_id = params[:item_id]
 #     @order.save
-#     redirect_to pages_orders_path
+     redirect_to show_order_path
    end
 
    def update
 #     @order = Order.find(params[:id])
-#     @order.update(deleted: true)
+#    @order.update(deleted: true)
 #     redirect_to pages_orders_owners_path
    end
 
    def destroy
 #     # @order = Order.find(params[:id])
 #     # @order.update(deleted: true)
-#     # redirect_to pages_orders_path
+      redirect_to show_orders_path
    end
 
    def show
 #     @order = Order.find(params[:id])
-#     @review = Review.new
+     @review = Review.new
    end
 
    def confirm
