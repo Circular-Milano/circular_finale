7c5574ce6e282dd79711b7695bd5803af97e0222
peter westman <pwestman@gmail.com>
2020-06-30 15:45:16 +0200

    add columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_100854) do
+ActiveRecord::Schema.define(version: 2020_06_30_132709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2020_06_30_100854) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
+    t.string "phone_number"
+    t.string "stripe_account"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
