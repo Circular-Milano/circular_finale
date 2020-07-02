54052c81f953947be42cb9f4a3e387f876710dd8
Massimo Galeotti <massi.galex@gmail.com>
2020-07-02 16:41:56 +0200

    navbar in progress

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,9 +3,9 @@ const initUpdateNavbarOnScroll = () => {
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-lewagon-grey');
+        navbar.classList.add('navbar-lewagon-black');
       } else {
-        navbar.classList.remove('navbar-lewagon-grey');
+        navbar.classList.remove('navbar-lewagon-black');
       }
     });
   }
