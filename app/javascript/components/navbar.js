f756b69eb6ea8f1b178bbd7c72ce2b6badc5fd42
Fernando Lazzarin <fernandolazzarin@yahoo.com.br>
2020-07-10 22:05:57 +0200

    many fixes

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= navbar.offsetHeight * 4) {
         navbar.classList.add('navbar-lewagon-black');
       } else {
         navbar.classList.remove('navbar-lewagon-black');
@@ -10,5 +10,17 @@ const initUpdateNavbarOnScroll = () => {
     });
   }
 }
+// const initUpdateNavbarOnScroll = () => {
+//   const navbar = document.querySelector('.navbar-lewagon');
+//   if (navbar) {
+//     window.addEventListener('scroll', () => {
+//       if (window.scrollY >= window.innerHeight) {
+//         navbar.classList.add('navbar-lewagon-black');
+//       } else {
+//         navbar.classList.remove('navbar-lewagon-black');
+//       }
+//     });
+//   }
+// }
 
 export { initUpdateNavbarOnScroll };
